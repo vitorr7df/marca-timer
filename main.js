f39093a2af678e4b91c7e6add2ae5db48792a65b
vitorr7df <vitor.rochadf@gmail.com>
2023-01-24 14:42:08 -0300

    Ajustando mainWindow

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Olá Mundo");
+const { app, BrowserWindow } = require('electron')
+
+app.on('ready', () => {
+    console.log('Aplicacão iniciada');
+    let mainWindow = new BrowserWindow({
+        webPreferences: {
+               nodeIntegration: true,
+               contextIsolation: false,
+               enableRemoteModule: true,
+        },
+        width: 600,
+        height: 400
+    });
+
+    mainWindow.loadURL('https://www.electronjs.org/docs/latest/');
+
+});

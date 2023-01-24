1823c3f450a797197a63428d15fa9f39f9cf936a
vitorr7df <vitor.rochadf@gmail.com>
2023-01-24 17:40:12 -0300

    Adc janelas

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,1 +1,7 @@
-console.log('Olá eu estou na  janela do Browser');
+const { ipcRenderer } = require('electron');
+
+let linkSobre = document.querySelector('#link-sobre');
+
+linkSobre.addEventListener('click' , function(){
+    ipcRenderer.send('abrir-janela-sobre');
+})
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 app.on('ready', () => {
     console.log('Aplicacão iniciada');
@@ -18,4 +18,14 @@ app.on('ready', () => {
 
 app.on('window-all-closed', () => {
     app.quit();
-})
+});
+
+ipcMain.on('abrir-janela-sobre', () => {
+   let sobreWindow = new BrowserWindow({
+    width: 300,
+    height: 200
+   });
+   
+   sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`);
+
+});
